19786e1fdc9437e5b03b84edbedd6c0f2b14a858
Galih <galihkur29@gmail.com>
2019-08-13 15:19:43 +0700

    add

--- src/app/server.js
+++ src/app/server.js
@@ -3,11 +3,11 @@ const path = require('path');
 const app = express();
 
 // Serve static files....
-app.use(express.static(__dirname + '/dist/angular-deploy'));
+app.use(express.static('dist/angular-deploy'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/angular-deploy/index.html'));
+  res.sendFile(path.join('dist/angular-deploy/index.html'));
 });
 
 // default Heroku PORT
